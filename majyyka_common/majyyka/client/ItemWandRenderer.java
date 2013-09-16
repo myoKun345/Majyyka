188f4dec4713cf2ebfdaff9893085ee21a958719
Myo-kun <myo.kun345@gmail.com>
2013-09-16 14:14:41 -0400

    Fixed file comments

--- majyyka_common/majyyka/client/ItemWandRenderer.java
+++ majyyka_common/majyyka/client/ItemWandRenderer.java
@@ -1,7 +1,7 @@
 /**
  * Majyyka
  *
- * ItemWandRenderer.scala
+ * ItemWandRenderer.java
  *
  * @author Myo-kun
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
--- majyyka_common/majyyka/client/ModelWand.java
+++ majyyka_common/majyyka/client/ModelWand.java
@@ -1,7 +1,7 @@
 /**
  * Majyyka
  *
- * ModelWand.scala
+ * ModelWand.java
  *
  * @author Myo-kun
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
